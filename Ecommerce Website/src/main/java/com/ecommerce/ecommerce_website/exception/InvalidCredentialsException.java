fb449f9e523964e43384a0882dd73ac7c17ff0ae
Odulapalli Hitesh <odulapallihitesh369@gmail.com>
2025-02-26 17:27:27 +0530

    -  Added InvalidCredentialsException in the exception package

---
+++ Ecommerce Website/src/main/java/com/ecommerce/ecommerce_website/exception/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package com.ecommerce.ecommerce_website.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+    
+}
