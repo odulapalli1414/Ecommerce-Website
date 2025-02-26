de00c22f889682f735102a897f6fa1ea416b897a
Odulapalli Hitesh <odulapallihitesh369@gmail.com>
2025-02-26 17:25:54 +0530

    - Added NotFoundException.java in the exception Package

---
+++ Ecommerce Website/src/main/java/com/ecommerce/ecommerce_website/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.ecommerce.ecommerce_website.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+    
+}
