6cc3b1cc415dadc9c57b66cd4e51593499870b58
Odulapalli Hitesh <odulapallihitesh369@gmail.com>
2025-02-28 11:51:57 +0530

    - Completed witht the Security and JWT token Configuration.

---
+++ Ecommerce Website/src/main/java/com/ecommerce/ecommerce_website/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.ecommerce.ecommerce_website.security;
+
+
+
+import com.ecommerce.ecommerce_website.entity.User;
+import com.ecommerce.ecommerce_website.exception.NotFoundException;
+import com.ecommerce.ecommerce_website.repository.UserRepo;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByEmail(username)
+                .orElseThrow(()-> new NotFoundException("User/ Email Not found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
